6ac1e17f0fc9ee14be3a711dbea6d035273f05a0
YUUKIToriyama <github@toriyama.dev>
2021-09-03 02:27:40 +0900

    郵便番号ごとにディレクトリを作成し、jsonファイルを出力

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,14 @@
+import * as fs from 'fs';
 import axios from 'axios';
 import * as AdmZip from 'adm-zip';
 import * as iconv from 'iconv-lite';
 import * as parse from 'csv-parse/lib/sync';
+
+fs.mkdir("./public", error => {
+	if (error) {
+		console.error("public/ is already exists");
+	}
+});
 
 const url = "https://www.post.japanpost.jp/zipcode/dl/kogaki/zip/26kyouto.zip";
 axios.get(url, {
@@ -14,16 +21,32 @@ axios.get(url, {
 	const entries = zip.getEntries();
 	entries.forEach(entry => {
 		const file = iconv.decode(entry.getData(), 'Shift_JIS');
-		console.log(file);
-		console.log(entry.entryName);
-		console.log(buffer.length);
-
-		const csv = parse(file, {
+		const csv: string[][] = parse(file, {
 			delimiter: ",",
 			onRecord: (records) => {
 				return records.slice(2, 9)
 			}
 		});
-		console.log(csv);
+		csv.forEach(row => {
+			const zipCode = row[0];
+			fs.mkdir("./public/" + zipCode.slice(0, 3), () => {
+				return true;
+			});
+			const json = {
+				zipCode: zipCode,
+				pref: row[4],
+				prefKana: row[1],
+				city: row[5],
+				cityKana: row[2],
+				town: row[6],
+				townKana: row[3]
+			};
+			fs.writeFile("./public/" + zipCode.slice(0, 3) + "/" + zipCode.slice(3, 7) + ".json", JSON.stringify(json), error => {
+				if (error) {
+					throw Error;
+				}
+			});
+		})
+
 	});
 });
