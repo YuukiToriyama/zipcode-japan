b303c3dae1e148c5d6aa3075b04aceb77d0ea90d
YUUKIToriyama <github@toriyama.dev>
2021-09-03 01:47:54 +0900

    iconvでShift_JISを変換し、csv-parseでパースする

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import axios from 'axios';
 import * as AdmZip from 'adm-zip';
+import * as iconv from 'iconv-lite';
+import * as parse from 'csv-parse/lib/sync';
 
 const url = "https://www.post.japanpost.jp/zipcode/dl/kogaki/zip/26kyouto.zip";
 axios.get(url, {
@@ -11,8 +13,17 @@ axios.get(url, {
 	const zip = new AdmZip(buffer);
 	const entries = zip.getEntries();
 	entries.forEach(entry => {
-		const file = entry.getData();
+		const file = iconv.decode(entry.getData(), 'Shift_JIS');
+		console.log(file);
 		console.log(entry.entryName);
 		console.log(buffer.length);
-	})
+
+		const csv = parse(file, {
+			delimiter: ",",
+			onRecord: (records) => {
+				return records.slice(2, 9)
+			}
+		});
+		console.log(csv);
+	});
 });
